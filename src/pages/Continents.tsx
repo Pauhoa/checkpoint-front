200f4365fcf44d2767773c312d18692a1d651c04
pauhoa <hoarau.paul3@gmail.com>
2023-05-22 15:10:14 +0200

    ✨ Page continents done
     Added apollo client config and query to get continents

--- src/pages/Continents.tsx
+++ src/pages/Continents.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { GET_CONTINENTS } from '../gql/queries';
+
+interface IContinent {
+    code: string;
+    name: string;
+}
 
 function Continents() {
+    const { loading, error, data } = useQuery(GET_CONTINENTS);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error</div>;
+    }
+
+    const continents = data.continents;
+
     return (
         <div>
-            Liste des continents
+            <h1>Continents</h1>
+            <ul>
+                {continents.map((continent: IContinent) => (
+                    <li key={continent.code}>
+                        <Link to={`/continents/${continent.code}`}>{continent.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
